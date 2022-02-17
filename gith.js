0c54c420229939a38c9d02d8718111a50229fef5
kot-katso <83894353+kot-katso@users.noreply.github.com>
2022-02-17 06:54:40 -0800

    first commit

---
+++ gith.js
@@ -0,0 +1,23 @@
+async function FetchGithubUsers() {
+
+    let get = await fetch(`
+
+    https://api.github.com/search/users?q=kout&in:user&page=34
+
+    `, {
+        headers: {
+            'Authorization': {
+            'clientID': 'dfc2b0b60896724527e6',
+            'clientSecret': 'a08482c88376441a2b3a79a1984feac0720ba1cf'
+        }
+        }
+    });
+    let response = await get.json();
+
+    let data = await response
+
+    console.log(data);
+
+}
+
+FetchGithubUsers();
