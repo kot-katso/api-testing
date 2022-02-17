876e4a4bd100c58622c40d45fdfb94ffd739c707
kot-katso <83894353+kot-katso@users.noreply.github.com>
2022-02-17 07:03:17 -0800

    update

--- gith.js
+++ gith.js
@@ -6,9 +6,7 @@ async function FetchGithubUsers() {
 
     `, {
         headers: {
-            'Authorization': {
-            'clientID': 'dfc2b0b60896724527e6',
-            'clientSecret': 'a08482c88376441a2b3a79a1984feac0720ba1cf'
+            'u': 'dfc2b0b60896724527e6:a08482c88376441a2b3a79a1984feac0720ba1cf'
         }
         }
     });
